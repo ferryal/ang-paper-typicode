f50cb39f712ea5d365e2d73811e1d9e4b4fe7c22
Ferryal Fajar <ferryal21@gmail.com>
2025-01-06 12:27:19 +0700

    feat: add component user list

---
+++ src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { User } from '../../models/user.model';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+})
+export class UserListComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
+}
